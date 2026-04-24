7d4be12af426223b9dbe19349263b9fab452f8b3
deibyvt <deibyvt@gmail.com>
2026-04-24 11:43:04 -0500

    Inicio aplicacion, paquetes del spring initializr

---
+++ MontoApp/MontoApp/src/main/java/com/example/MontoApp/MontoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MontoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontoAppApplication.class, args);
+	}
+
+}
---
+++ MontoApp/MontoApp/src/test/java/com/example/MontoApp/MontoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MontoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
